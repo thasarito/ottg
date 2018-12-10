108c28e94741753e511f75cad3c56acecac92f26
thasarito <thasarito@thasaritos-MacBook-Pro.local>
2018-12-10 22:09:16 +0700

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 import unittest
+import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,28 +20,44 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		# She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attrivute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
+
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an iten in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assrtTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
+		# There is still a text box inviting her to add another item. She
+		# enters "User peacock feathers to make a fly" (Edith is very methodical)
 		self.fail('Finish the test!')
 
-# She is invited to enter a to-do item straight away
+		# The page updates again, and now shows both items on her list
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an iten in a to-do list
+		# She visits that URL - her to-do list is still there.
 
-# There is still a text box inviting her to add another item. She
-# enters "User peacock feathers to make a fly" (Edith is very methodical)
-
-# The page updates again, and now shows both items on her list
-
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect
-
-# She visits that URL - her to-do list is still there.
-
-# Satidfied, she goes back to sleep
+		# Satidfied, she goes back to sleep
 
 if __name__ =='__main__':
 	unittest.main()
